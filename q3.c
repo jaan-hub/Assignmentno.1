c617db8283223294c195ac5f7e61068258e7812c
faijan pathan <official.jaan07@gmail.com>
2025-07-14 14:38:29 +0530

    Commited

---
+++ q3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float fahrenheit, celsius, kelvin;
+
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    celsius = (5.0 / 9.0) * (fahrenheit - 32);
+    kelvin = celsius + 273.15;
+    printf("Temperature in Celsius: %.2f °C\n", celsius);
+    printf("Temperature in Kelvin: %.2f K\n", kelvin);
+
+    return 0;
+}
